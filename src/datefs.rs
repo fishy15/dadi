9cc86166fd42f8da4a9d588d09f196b4c61d4627
= <38575855+fishy15@users.noreply.github.com>
2025-09-06 02:32:32 -0400

    Update date format to ISO standard

--- src/datefs.rs
+++ src/datefs.rs
@@ -1,57 +1,51 @@
 use std::fs;
 
-use std::ffi::OsString;
+use std::ffi::{OsStr, OsString};
 use std::io::Error;
 use std::path::Path;
+use time::{Date, Month};
 
 #[derive(Debug)]
 pub enum DateFSError {
     BaseMissing,
-    InvalidFolder(OsString),
+    EmptyDir,
+    InvalidDate(i32, u8, u8),
+    InvalidFile(OsString),
     OSError(Error),
 }
 
-// The previous number that occurs before the current one in the file system.
-// Also checks to make sure that there are only integer file names present.
-fn previous_before_int(base_path: &Path, bound: u32) -> Result<Option<u32>, DateFSError> {
+// The previous date that occurs before the current one in the file system.
+// Also does checks to make sure that all files are valid.
+// Returns None if there was no previous date
+fn previous_before(base_path: &Path, bound: Date) -> Result<Option<Date>, DateFSError> {
     let paths = fs::read_dir(base_path).map_err(|_| DateFSError::BaseMissing)?;
-    let folder_names = paths
-        .map(|p| match p {
-            Ok(path) => path
-                .file_name()
-                .into_string()
-                .map_err(|_| DateFSError::InvalidFolder(path.path().into_os_string()))?
-                .parse::<u32>()
-                .map_err(|_| DateFSError::InvalidFolder(path.path().into_os_string())),
-            Err(e) => Err(DateFSError::OSError(e)),
-        })
-        .collect::<Result<Vec<u32>, DateFSError>>()?;
-    return Ok(folder_names.into_iter().filter(|name| *name < bound).max());
+    let file_dates = paths
+        .map(|p|
+            match p {
+                Ok(path) => Ok(extract_date(path.path().as_path())?),
+                Err(e) => Err(DateFSError::OSError(e)),
+            })
+            .collect::<Result<Vec<Date>, DateFSError>>()?;
+
+    let max_date = file_dates.into_iter().filter(|d| *d < bound).max();
+    return Ok(max_date);
 }
 
-fn previous_year(base_path: &Path, year: u32) -> Result<Option<u32>, DateFSError> {
-    return previous_before_int(base_path, year);
+fn extract_date(path: &Path) -> Result<Date, DateFSError> {
+    let err = DateFSError::InvalidFile(path.as_os_str().into());
+
+    if path.extension() == Some(&OsStr::new("md")) {
+        if let Some(stem) = path.file_stem().and_then(|s| s.to_str()) {
+            let format = time::format_description::well_known::Iso8601::DATE;
+            let date = Date::parse(stem, &format).map_err(|_| err)?;
+            return Ok(date);
+        }
+    }
+
+    return Err(err);
 }
 
-fn previous_month(base_path: &Path, year: u32, month: u32) -> Result<Option<u32>, DateFSError> {
-    let mut new_base = base_path.to_path_buf();
-    new_base.push(year.to_string());
-    return previous_before_int(new_base.as_path(), month);
-}
-
-fn previous_date(
-    base_path: &Path,
-    year: u32,
-    month: u32,
-    day: u32,
-) -> Result<Option<u32>, DateFSError> {
-    let mut new_base = base_path.to_path_buf();
-    new_base.push(year.to_string());
-    new_base.push(month.to_string());
-    return previous_before_int(new_base.as_path(), day);
-}
-
-pub fn date_exists(base_path: &Path, year: u32, month: u32, date: u32) -> bool {
+pub fn date_exists(base_path: &Path, year: i32, month: u32, date: u32) -> bool {
     let mut path_buf = base_path.to_path_buf();
     path_buf.push(year.to_string());
     path_buf.push(month.to_string());
@@ -59,33 +53,59 @@ pub fn date_exists(base_path: &Path, year: u32, month: u32, date: u32) -> bool {
     return path_buf.exists();
 }
 
-macro_rules! assert_ok_eq {
-    ($left:expr, $right:expr) => {
-        match $right {
-            Ok(res) => assert_eq!($left, res),
-            Err(e) => panic!("e2 return Err: {:?}", e),
-        }
-    };
-}
-
 #[cfg(test)]
 mod test {
     use super::*;
 
+    macro_rules! assert_ok_eq {
+        ($left:expr, $right:expr) => {
+            match $right {
+                Ok(res) => assert_eq!($left, res),
+                Err(e) => panic!("e2 return Err: {:?}", e),
+            }
+        };
+    }
+
+    fn date(year: i32, month: u8, date: u8) -> Date {
+        let month_enum = Month::January.nth_next(month - 1);
+        return Date::from_calendar_date(year, month_enum, date).unwrap();
+    }
+
     static BASE_PATH: &str = "./test-dirs/";
 
     #[test]
-    fn test1() {
+    fn test_previous() {
         let base_path = Path::new(BASE_PATH).join("test1");
-        assert_ok_eq!(None, previous_year(&base_path, 2016));
-        assert_ok_eq!(None, previous_year(&base_path, 2017));
-        assert_ok_eq!(Some(2018), previous_year(&base_path, 2019));
-        assert_ok_eq!(Some(2018), previous_year(&base_path, 2020));
-        assert_ok_eq!(Some(2020), previous_year(&base_path, 2021));
-        assert_ok_eq!(Some(2020), previous_year(&base_path, 2022));
-        assert_ok_eq!(Some(2020), previous_year(&base_path, 2023));
-        assert_ok_eq!(Some(2020), previous_year(&base_path, 2024));
-        assert_ok_eq!(Some(2024), previous_year(&base_path, 2025));
-        assert_ok_eq!(Some(2025), previous_year(&base_path, 2026));
+
+        assert_ok_eq!(None, previous_before(&base_path, date(2017, 1, 1)));
+        assert_ok_eq!(None, previous_before(&base_path, date(2018, 1, 1)));
+        assert_ok_eq!(None, previous_before(&base_path, date(2018, 3, 28)));
+        assert_ok_eq!(None, previous_before(&base_path, date(2018, 3, 29)));
+        assert_ok_eq!(Some(date(2018, 3, 29)), previous_before(&base_path, date(2018, 3, 30)));
+        assert_ok_eq!(Some(date(2018, 3, 29)), previous_before(&base_path, date(2018, 4, 1)));
+        assert_ok_eq!(Some(date(2018, 3, 29)), previous_before(&base_path, date(2018, 4, 28)));
+        assert_ok_eq!(Some(date(2018, 3, 29)), previous_before(&base_path, date(2018, 4, 29)));
+        assert_ok_eq!(Some(date(2018, 4, 29)), previous_before(&base_path, date(2018, 4, 30)));
+        assert_ok_eq!(Some(date(2018, 4, 30)), previous_before(&base_path, date(2018, 5, 1)));
+        assert_ok_eq!(Some(date(2018, 4, 30)), previous_before(&base_path, date(2018, 5, 2)));
+        assert_ok_eq!(Some(date(2018, 4, 30)), previous_before(&base_path, date(2018, 6, 2)));
+        assert_ok_eq!(Some(date(2018, 4, 30)), previous_before(&base_path, date(2018, 12, 31)));
+        assert_ok_eq!(Some(date(2018, 4, 30)), previous_before(&base_path, date(2019, 1, 1)));
+        assert_ok_eq!(Some(date(2019, 1, 1)),  previous_before(&base_path, date(2019, 1, 2)));
+        assert_ok_eq!(Some(date(2019, 1, 1)),  previous_before(&base_path, date(2019, 1, 3)));
+        assert_ok_eq!(Some(date(2019, 1, 1)),  previous_before(&base_path, date(3019, 1, 3)));
+    }
+
+    #[test]
+    fn test_missing_md() {
+        let base_path = Path::new(BASE_PATH).join("test2");
+
+        fn date(year: i32, month: u8, date: u8) -> Date {
+            let month_enum = Month::January.nth_next(month - 1);
+            return Date::from_calendar_date(year, month_enum, date).unwrap();
+        }
+
+        assert!(previous_before(&base_path, date(2018, 3, 20)).is_err());
+        assert!(previous_before(&base_path, date(2019, 3, 20)).is_err());
     }
 }
